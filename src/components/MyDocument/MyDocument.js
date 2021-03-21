8383bc2e53ed9235d3596a1a201402b5eb9901e7
Satheesh Natarajan <satheeshnatarajan@outlook.com>
2021-03-21 20:54:22 +0530

    Update MyDocument.js

--- src/components/MyDocument/MyDocument.js
+++ src/components/MyDocument/MyDocument.js
@@ -4,38 +4,68 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 // Create styles
 const styles = StyleSheet.create({
-  center: {
-    textAlign: 'center',
-  },
   page: {
     fontSize: '12',
-    padding: 30,
+    padding: 40,
   },
   table: {
     display: 'flex',
     flexDirection: 'row',
-    marginTop: 20,
+  },
+  tableCol: {
+    display: 'flex',
+    flexDirection: 'column',
   },
   section: {
     flexGrow: 1,
+  },
+  center: {
+    textAlign: 'center',
+  },
+  flexCenter: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  border: {
+    borderWidth: 1,
   },
   chip: {
     padding: '3',
     color: '#e0e0e0',
   },
   eligible: {
-    color: '#3f51b5',
-  },
-  formControl: {
-    display: 'flex',
-    flexDirection: 'row',
+    color: '#ffffff',
+    backgroundColor: '#3f51b5',
   },
   label: {
     padding: '8 4',
-    // borderWidth: '1',
   },
-  span2: {
-    flexGrow: 4,
+  registration: {
+    backgroundColor: '#f8bbd0',
+  },
+  marks: {
+    backgroundColor: '#b3e5fc',
+  },
+  groupElg: {
+    backgroundColor: '#d7ccc8',
+  },
+  remarks: {
+    backgroundColor: '#ffe0b2',
+  },
+  language: {
+    backgroundColor: '#ce93d8',
+  },
+  maths: {
+    backgroundColor: '#a5d6a7',
+  },
+  science: {
+    backgroundColor: '#cfd8dc',
+  },
+  total: {
+    backgroundColor: '#eceff1',
+  },
+  consess: {
+    backgroundColor: '#90caf9',
   },
 });
 
@@ -43,75 +73,105 @@ const styles = StyleSheet.create({
 const MyDocument = ({ data }) => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.table}>
+      <View style={[styles.table, styles.border]}>
+        <View style={[styles.tableCol]}>
+          <Text style={[styles.label, styles.center]}>
+            MOUNT ZION MATRICULATION HIGHER SECONDARY SCHOOL
+          </Text>
+          <Text style={[styles.label, styles.center]}>ACADEMIC YEAR 2021-2022</Text>
+        </View>
+      </View>
+      <View style={[styles.table, styles.border, styles.registration]}>
         <Text style={[styles.label, styles.center]}>XI Registration Particular</Text>
       </View>
       <View style={styles.table}>
-        <View style={styles.section}>
-          <Text style={styles.label}>Student Name: </Text>
-          <Text style={styles.label}>Father&apos;s Name: </Text>
-          <Text style={styles.label}>Mother&apos;s Name: </Text>
-          <Text style={styles.label}>Mobile no.: </Text>
-          <Text style={styles.label}>Whatsapp no.: </Text>
-          <Text style={styles.label}>Previous School: </Text>
+        <View style={[styles.table, styles.border]}>
+          <View style={styles.section}>
+            <Text style={styles.label}>Student Name: </Text>
+            <Text style={styles.label}>Father&apos;s Name: </Text>
+            <Text style={styles.label}>Mother&apos;s Name: </Text>
+            <Text style={styles.label}>Mobile no.: </Text>
+            <Text style={styles.label}>Whatsapp no.: </Text>
+            <Text style={styles.label}>Previous School: </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.label}>{data.studentName}</Text>
+            <Text style={styles.label}>{data.fatherName}</Text>
+            <Text style={styles.label}>{data.motherName}</Text>
+            <Text style={styles.label}>{data.mobileNo}</Text>
+            <Text style={styles.label}>{data.whatsAppNo}</Text>
+            <Text style={styles.label}>{data.previousSchool}</Text>
+          </View>
         </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>{data.studentName}</Text>
-          <Text style={styles.label}>{data.fatherName}</Text>
-          <Text style={styles.label}>{data.motherName}</Text>
-          <Text style={styles.label}>{data.mobileNo}</Text>
-          <Text style={styles.label}>{data.whatsAppNo}</Text>
-          <Text style={styles.label}>{data.previousSchool}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Application Date: </Text>
-          <Text style={styles.label}>Application No: </Text>
-          <Text style={styles.label}>Entrance Date: </Text>
-          <Text style={styles.label}>Address:</Text>
-          <Text style={[styles.label, styles.span2]}>{data.address}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>{new Date(data.applicationDate).toLocaleString()}</Text>
-          <Text style={styles.label}>{data.applicationNo}</Text>
-          <Text style={styles.label}>{new Date(data.applicationDate).toLocaleString()}</Text>
+        <View style={[styles.tableCol, styles.border, styles.section]}>
+          <View style={styles.table}>
+            <View>
+              <Text style={styles.label}>Application Date: </Text>
+              <Text style={styles.label}>Application No: </Text>
+              <Text style={styles.label}>Entrance Date: </Text>
+            </View>
+            <View>
+              <Text style={styles.label}>{new Date(data.applicationDate).toLocaleString()}</Text>
+              <Text style={styles.label}>{data.applicationNo}</Text>
+              <Text style={styles.label}>{new Date(data.applicationDate).toLocaleString()}</Text>
+            </View>
+          </View>
+          <View style={styles.tableCol}>
+            <Text style={styles.label}>Address:</Text>
+            <Text style={styles.label}>{data.address}</Text>
+          </View>
         </View>
       </View>
-
-      <View style={styles.table}>
+      <View style={[styles.table, styles.border, styles.marks]}>
         <Text style={[styles.label, styles.center]}>Marks</Text>
       </View>
-
       <View style={styles.table}>
-        <View style={styles.section}>
-          <Text style={styles.label}>Language: </Text>
-          <Text style={styles.label}>Maths: </Text>
-          <Text style={styles.label}>Science: </Text>
-          <Text style={styles.label}>Total: </Text>
+        <View style={[styles.table, styles.border]}>
+          <View style={styles.section}>
+            <Text style={[styles.label, styles.language]}>Language: </Text>
+            <Text style={[styles.label, styles.maths]}>Maths: </Text>
+            <Text style={[styles.label, styles.science]}>Science: </Text>
+            <Text style={[styles.label, styles.total]}>Total: </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={[styles.label, styles.language]}>{data.marks.language}</Text>
+            <Text style={[styles.label, styles.maths]}>{data.marks.maths}</Text>
+            <Text style={[styles.label, styles.science]}>{data.marks.science}</Text>
+            <Text style={[styles.label, styles.total]}>{data.marks.total}</Text>
+          </View>
         </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>{data.marks.language}</Text>
-          <Text style={styles.label}>{data.marks.maths}</Text>
-          <Text style={styles.label}>{data.marks.science}</Text>
-          <Text style={styles.label}>{data.marks.total}</Text>
-        </View>
-        <View style={[styles.section, styles.span2]}>
-          <Text style={styles.label}>Concession: {data.marks.concession}%</Text>
-          <Text style={styles.label}>Group Eligible:</Text>
-          {data.marks.courses.map((course) => {
-            const style = course.eligible ? [styles.chip, styles.eligible] : [styles.chip];
-            return (
-              <Text key={course.code} style={style}>
-                {course.code}
+        <View style={[styles.tableCol, styles.section]}>
+          <View style={styles.table}>
+            <View style={[styles.table, styles.section, styles.border]}>
+              <Text style={[styles.label, styles.section]}>Concession:</Text>
+              <Text style={[styles.label, styles.section, styles.consess]}>
+                {data.marks.concession}%
               </Text>
-            );
-          })}
+            </View>
+          </View>
+          <View style={[styles.table, styles.groupElg, styles.border, styles.flexCenter]}>
+            <Text style={[styles.label]}>Group Eligibility</Text>
+          </View>
+          <View style={[styles.table, styles.border, styles.section]}>
+            {data.marks.courses.map((course) => {
+              const style = [styles.table, styles.section, styles.flexCenter];
+              course.eligible ? style.push(styles.eligible) : '';
+              return (
+                <View key={course.code} style={style}>
+                  <Text key={course.code} style={[styles.chip, styles.label]}>
+                    {course.code}
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
         </View>
       </View>
-      <View style={styles.table}>
-        <View style={styles.section}>
-          <Text style={[styles.label]}>Remarks:</Text>
-          <Text style={styles.label}>{data.remarks}</Text>
-        </View>
+      <View style={[styles.table, styles.border, styles.remarks]}>
+        <Text style={[styles.label, styles.center]}>Remarks</Text>
+      </View>
+      <View style={[styles.table, styles.border]}>
+        <Text style={styles.label}>{data.remarks}</Text>
       </View>
     </Page>
   </Document>
